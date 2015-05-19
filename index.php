b01f8096ab3ac53903e99a037f70a032bdc79b06
Emily <espatonay@gmail.com>
2015-05-19 13:59:17 -0700

    Commit for functional app

---
+++ index.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <title>Manual Differential Counter</title>
+   <link rel="stylesheet" type="text/css" href="styles/landing-style.css">
+   <link rel="stylesheet" type="text/css" href="styles/counter.css">
+
+</head>
+
+<body>
+
+<!-- Masthead of my pathology pages goes here. -->
+
+<div class='header-cont'>
+	<?php
+		//putting in my header
+
+		include_once __DIR__ . "/styles/masthead.html";
+
+	?>
+
+</div>
+
+<div class='content-body'>
+    <h1>Manual Differential Counter</h1>
+<!-- The counter code starts here. -->
+<div class = 'div-counter effect8'>
+<form><table id='counter'>
+   <th>
+<?php
+   $arCells =array(
+       'A', 'S', 'D', 'F',
+       'Z', 'X', 'C', 'V', 'B');
+   $arCells2 = array ( // This is the setup I learned from SDH.
+       'cellA' => 'blast',
+       'cellS' => 'pro',
+       'cellD' => 'meta',
+       'cellF' => 'erythro',
+       'cellZ' => 'baso',
+       'cellX' => 'eos',
+       'cellC' => 'plasma',
+       'cellV' => 'lymph',
+       'cellB' => 'mono',
+       'celltot' => 'total'
+   );
+
+   foreach ($arCells2 as $key => $celltype) { ?>
+
+       <td class='namecell' id='<?php echo $key ?>'></td>
+
+       <?php } ?>
+   </th>
+   <tr>
+   		<td class="datacell" id='row-label'>Number:</td> <!--this is to help user seee the row
+        			is the raw number -->
+   <?php foreach ($arCells2 as  $key => $celltype2) { ?>
+
+       <td class='datacell'><input class='cellAmount' type='number'id='num<?php echo $key?>'></td>
+
+   <?php } ?>
+   </tr>
+   <tr>
+   		<td class='percentcell' id='row-label'>Percent (&#37;):</td>
+   <?php foreach ($arCells2 as $key => $celltype2) { ?>
+
+<td class='percentcell' ><span type='number' class='cellPercent' id='percent<?php echo $key?>'></span></td>
+
+   <?php } ?>
+   </tr>
+
+   <tr>
+   		<td class='namecell' id='row-label'>Key:</td>
+		<?php foreach ($arCells as $aLetter) { ?>
+   		<td class = "namecell"><?php echo $aLetter ?></td>
+   		<?php } ?>
+   </tr>
+</table></form>
+
+
+<div id = 'buttons'>
+    <label for="specimenType">Specimen Type</label>
+    <select id="specimenType">
+        <option value="bm" selected="selected">Bone Marrow</option>
+        <option value="pb">Peripheral Blood</option>
+    </select>
+    <button id='btnStartCount'>Start Count</button>
+    <button id='btnCountDone'>Count Done</button>
+</div>
+</div> <!-- div-counter -->
+
+
+<div class="output">
+
+<!--This is where the actual tabs will live as an ul.-->
+<!--Credit for this code belongs to a snipet I found on Codepen.-->
+    <ul class="tabs">
+        <li class="tab-link current" data-tab="tab-1">YSM Template</li>
+        <li class="tab-link" data-tab="tab-2">PDX Template</li>
+	<li class="tab-link" data-tab="tab-3">MGH Template</li>
+	<li class="tab-link" data-tab="tab-4">Periph Blood</li>
+    </ul>
+
+    <div id='tab-1' class="tab-content current">
+        <p class="instructions">Instructions:<p>
+        <p>Click &ldquo;Start Count&rdquo; to get going.
+        <p>Once count is done, click &ldquo;Count Done&rdquo; to write results.</p>
+    </div>
+
+    <div id='tab-2' class="tab-content">
+        <span id='pdxOut'>
+            <p class="instructions">Instructions:<p>
+            <p>Click &ldquo;Start Count&rdquo; to get going.
+            <p>Once count is done, click &ldquo;Count Done&rdquo; to write results.</p>
+        </span>
+    </div>
+
+    <div id='tab-3' class="tab-content">
+        <p class="instructions">Instructions:<p>
+        <p>Click &ldquo;Start Count&rdquo; to get going.
+        <p>Once count is done, click &ldquo;Count Done&rdquo; to write results.</p>
+    </div>
+
+    <div id='tab-4' class='tab-content' style="text-align:center">
+        <span id='gpbOut'><h3>This feature is not yet active.</h3></span><br>
+        <img id='inactive' src="images/puppies.jpg">
+    </div>
+
+</div><!-- output -->
+
+</div><!--content body-->
+<!--
+	Below is the script to run the counter. I hope.
+
+-->
+<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+<script src="scripts/keypress.js"></script>
+<script src="scripts/counter.js"></script>
+</body>
+</html>
---
+++ scripts/counter.js
@@ -0,0 +1,234 @@
+// JavaScript Document
+
+// Things that need to be defined first
+// Getting the app ready to load the template JSON
+
+var xhr = new XMLHttpRequest();
+var jsonObj = {};
+
+//this function is to change between bm and pb
+
+xhrOut = function (var1) {
+	this.outPut = var1;
+};
+
+xhr.onload = function() {
+	var tempVar = ''; responseObject = {};
+        textOut = '';
+    if(xhr.status === 200) {
+        textOut = xhr.responseText;
+        responseObject = JSON.parse(xhr.responseText);
+        ysmJsonObj = new xhrOut(responseObject.ysmTemplate);
+        pdxJsonObj = new xhrOut(responseObject.pdxTemplate);
+        jsonObj = new xhrOut(responseObject);
+
+        // In order to properly name the heading of each column, I'll gather
+        // the names of the columns from the cellCode part of the
+        // templates.JSON file by pulling them out of responseObject (where
+        // the JSON template object lives.
+        outCodes = responseObject.ysmTemplate.outCodes;
+
+        // Iterate through all the cell codes and rename the text in the
+        // column header. The php script above should name them according to the
+        // key strokes correlating to that cell.
+        for (var cellCode in outCodes) {
+            var tempVar = 'td#cell' + cellCode;
+            $(tempVar).text(outCodes[cellCode]);
+        }
+    }
+};
+
+function resetCounter () {
+	// This will allow the counters to all be set to 0
+	// to start the script
+	console.log('resetting counter');
+    $('.cellAmount').val(0);
+	$('.cellPercent').text(0);
+	$('#percentcelltot').text('100%');
+};
+
+function calcPercent(e) {
+        // get the outCodes object from the JSON object, putting this
+        // into a local variable objOutCodes.
+        var objOutcodes = ysmJsonObj.outPut.outCodes;
+	// This function recalculates the percentage for
+	// each bio cell type, when a key is pressed.
+	var percentCell = [];
+	var varStr = '';
+	for (var cellCode in objOutcodes) {
+		varStrCell = '#numcell' + cellCode;
+		varStrPercent = '#percentcell' + cellCode;
+		finalCellPercent = $(varStrCell).val() /
+						$('#numcelltot').val() * 100;
+		$(varStrPercent).text(finalCellPercent.toFixed(2) + "%");
+	};
+
+};
+
+function addToCell(whichCell) {
+	// This function takes the data from the listener
+	// below and adds one to each cell when that button
+	// is presesed
+	var wchCellStr = '';
+	var curAmount = 0; var newAmount = 0; curTot = 0; newTot = 0;
+	wchCellStr = "#numcell" + whichCell.toUpperCase();
+	curAmount = $(wchCellStr).val();
+
+	// Increase the chosen cell by one
+	newAmount = (curAmount * 1 + 1);
+	$(wchCellStr).val("");
+	$(wchCellStr).val(newAmount);
+
+	// Increase the total cell by one
+	curTot = $('#numcelltot').val();
+	newTot = curTot * 1 + 1;
+	$('#numcelltot').val(newTot);
+	calcPercent();
+};
+
+function makeOutput(e) {
+    // First pull in the modelSentence from the JSON object.
+
+    var modelSentence = jsonObj.outPut[e + 'Template'];
+    modelSentence = modelSentence.outSentence;
+    // Then, pull back in the object with the cell type and codes.
+    // The cell type abbreviation should match the <span> ids in the
+    // modelSentence.
+    var objOutcodes = jsonObj.outPut[e + 'Template'];
+    objOutcodes = objOutcodes.outCodes;
+    var tmpStr = 'span#' + e + 'Out'; console.log(tmpStr);
+
+    $(tmpStr).html(modelSentence);  // Print out the empty model sentances.
+
+    // now employ a for-in loop to write the results to the modelSentence
+    for (var cellCode in objOutcodes) {
+        var varStrCell = '#numcell' + cellCode;
+	var varStrPercent = '#percentcell' + cellCode;
+	var finalCellPercent = $(varStrCell).val() /
+					$('#numcelltot').val() * 100;
+        var roundedCellPercent = (Math.round(finalCellPercent)).toString();
+        if (roundedCellPercent === '0') {
+            roundedCellPercent = '<1'; // All 0s made <1.
+        };
+       	var cellAbbr = 'span#' + e + objOutcodes[cellCode];
+        $(cellAbbr).html(roundedCellPercent);
+
+        // The current code will make the cell count always 100, since anything
+        // divided by itself times 100 is 100. So, got to put that in
+        // seperately.
+        var valOfTotal = $('#numcelltot').val();
+        var tmpStr = 'span#' + e + 'total';
+        $(tmpStr).html(valOfTotal);
+
+    };
+
+
+};
+
+function writeTblLbl(e1){
+    var locOutCodes; // a local scope variable that will hold the outCodes
+    //                  from the JSON file.
+    //
+    // The purpose of this function is to change the labels on the table.
+    // For now, there are two choices: pb (peripheral blood) and bm (bone
+    // marrow).
+
+    // The argument e1 will be either 'bm' or 'pb'.
+
+    locOutCodes = e1.outCodes;
+
+    for (var cellCode in locOutCodes) {
+        var tempVar = 'td#cell' + cellCode;
+        $(tempVar).text(locOutCodes[cellCode]);
+        };
+    };
+
+function updateBmPb(e){
+    console.log(e);
+    var tempObj = {}; var subTempObj = {}; var outObj = {};
+    for (var templateId in jsonObj){
+        tempObj = jsonObj[templateId];
+        for (var templateData in tempObj) {
+            subTempObj = tempObj[templateData];
+            if (subTempObj['specType'] === e){
+                writeTblLbl(subTempObj);
+                break;
+            }
+            else { /*do nothing*/}
+        };
+    };
+};
+
+
+$(document).ready(function() {
+    resetCounter();
+    xhr.open('POST', 'templates.json', true); // Open up the template file
+    xhr.send(null);                          // Send the request
+
+    	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+});
+
+$('button#btnStartCount').on('click', function(){
+      console.log('User clicked Start Count button.');
+
+      // Disabling start button because clicking twice will double the value of
+      // each key stroke.
+      document.getElementById('btnStartCount').disabled = true;
+
+      // Initiate listeners for the different keys I am going to use.
+      // Will need to figure out how to make a QWERTZ keyboard user able to
+      // make use of this all.
+      var listener = new window.keypress.Listener();
+	  listener.simple_combo("a", function(){addToCell('a')});
+	  listener.simple_combo("s", function(){addToCell('s')});
+	  listener.simple_combo("d", function(){addToCell('d')});
+	  listener.simple_combo("f", function(){addToCell('f')});
+	  listener.simple_combo("z", function(){addToCell('z')});
+	  listener.simple_combo("x", function(){addToCell('x')});
+	  listener.simple_combo("c", function(){addToCell('c')});
+	  listener.simple_combo("v", function(){addToCell('v')});
+	  listener.simple_combo("b", function(){addToCell('b')});
+
+	});
+
+$('button#btnCountDone').on('click', function(){
+    // This is the button to cause the output to be printed.
+	console.log('User clicked "btnCountDone"');
+	if ($('input#numtot').val() === 0) {
+		alert("Please include at least one cell. You're total so far is 0.");
+                location.reload();
+	};
+
+    calcPercent();
+    makeOutput('pdx'); makeOutput('ysm'); makeOutput('mgh');
+
+
+});
+
+$('select#specimenType').on('change', function (){
+    var locSpecType ='';
+    var strOutput = "The Specimen Type has been changed to ";
+    if (this.value === 'bm') {
+        strOutput += 'bone marrow.';
+        // If the value of the selection box gets changed to bm,
+        // then call the function that updates the counter and templates,
+        // passing 'bm' to that function.
+        locSpecType = 'bm';
+    }
+    else if (this.value === 'pb'){
+        // Otherwise, pass 'pb' to that function.
+        strOutput += 'peripheral blood.';
+        locSpecType = 'pb';
+    }
+    console.log(strOutput);
+    updateBmPb(locSpecType);
+});
