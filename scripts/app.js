c47c16b15e2dcbf95222a54b6e77d8d18529b20c
Emily Patonay <espatonay@gmail.com>
2015-05-20 13:46:12 -0400

    Another Backbone commit

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by emilypatonay on 5/19/15.
+ */
+
+// Define the app namespace
+
+var app = {
+    v: {type: 'views'},
+    r: {type: 'routers'},
+    c: {type: 'collections'},
+    m: {type: 'models'}
+};
---
+++ scripts/views.js
@@ -0,0 +1,4 @@
+/**
+ * Created by emilypatonay on 5/19/15.
+ */
+
