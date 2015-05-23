b1f6ffc80c86c441ae3d6aea00c75e1467a350a0
Emily Patonay <espatonay@gmail.com>
2015-05-23 07:32:11 +0200

    Some more refactoring, though not functional yet

--- scripts/models.js
+++ scripts/models.js
@@ -3,26 +3,8 @@
  */
 
 (function($){
-    app.m.KeysUsed = Backbone.Model.extend({
-        url: 'settings/keyboard.json',
-        initialize: function(){
-            this.tblData(); this.tblKeysUsed();
-            console.log(this);
-        },
-        tblData: function(){
-            // TODO: Verify that the keysUsed.length === outputTpl.length
-            // Put out the data in an object {keysUsed: [...], cellNames: [...]
-            var that = this;
-            $.getJSON('settings/outputTpl.json', function(data){
-                that.tblTpl = data;
-            });
-        },
-        tblKeysUsed: function(){
-            var that = this; that.keysUsed = {};
-            // Gets the JSON list of keys to be used for app on initialization
-            $.getJSON('settings/keyboard.json', function(data){
-                that.keysUsed = data; console.log(data === that.keysUsed);
-            });
-        }
+    app.m.KeysRow = Backbone.Model.extend({
+        url: 'settings/keyboard.json'
+
     });
 })(jQuery);
--- scripts/views.js
+++ scripts/views.js
@@ -3,16 +3,33 @@
  */
 
 (function($){
-    app.v.MakeTable = Backbone.View.extend({
-        tagName: 'td',
-        model: new app.m.KeysUsed(),
-        template: Handlebars.compile($('#table-tpl').html()),
-        target: $('#counter-tbl'), // This is where the rendered tpl will go
+    'use strict';
 
-        initialize: function(response) {
+    app.v.MkKeyRow = Backbone.View.extend({
+        // Using this to render the row of keys at the bottom of the app table.
+        tagName: 'tr',
+        id: 'keyRow',
+        initialize: function(response){
+            console.log(response);
+            return this;
+        }
+    });
+
+    app.v.MakeTable = Backbone.View.extend({
+        tagName: 'table',
+        id: 'counter-tbl',
+        className: 'table',
+        template: Handlebars.compile($('#table-tpl').html()),
+        el: $('#counter-tbl'), // This is where the rendered tpl will go
+
+        initialize: function() {
             // TODO: call in the data object from tblData
-            // TODO: render table
+            // TODO: render table one by one and then append to DOM
             // TODO: listenTo keypresses
+            this.model = new app.m.KeysRow();
+            console.log(this.model);
+            var mkKeyRow = new app.v.MkKeyRow({model: this.model});
+            console.log(mkKeyRow);
         }
     });
 })(jQuery);
