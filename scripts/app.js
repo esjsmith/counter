c8306d726ab10430420215fbc98dcc6d27143468
Emily Patonay <espatonay@gmail.com>
2015-05-28 09:15:34 +0200

    refactor extra functions into app.tools namespace

--- scripts/app.js
+++ scripts/app.js
@@ -6,24 +6,26 @@
 // HANDLEBARS HELPERS
 
 // OTHER FUNCTIONS
-function resetCounter () {
-    // This will allow the counters to all be set to 0
-    // to start the script
-    console.log('resetting counter');
-    $('.cellAmount').val(0);
-    $('.cellPercent').text(0);
-    $('#percentcelltot').text('100%');
-}
+(function($){
+    app.tools = {
+        tabbedOutput: function(){
+            $('ul.tabs li').click(function() {
+                var tab_id = $(this).attr('data-tab');
 
-function tabbedOutput(){
-console.log('here');
-    $('ul.tabs li').click(function() {
-        var tab_id = $(this).attr('data-tab');
+                $('ul.tabs li').removeClass('current');
+                $('.tab-content').removeClass('current');
 
-        $('ul.tabs li').removeClass('current');
-        $('.tab-content').removeClass('current');
-
-        $(this).addClass('current');
-        $("#" + tab_id).addClass('current');
-    })
-}
+                $(this).addClass('current');
+                $("#" + tab_id).addClass('current');
+            })
+        },
+        resetCounter: function(){
+            // This will allow the counters to all be set to 0
+            // to start the script
+            console.log('resetting counter');
+            $('.cellAmount').val(0);
+            $('.cellPercent').text(0);
+            $('#percentcelltot').text('100%');
+        }
+    };
+})(jQuery);
