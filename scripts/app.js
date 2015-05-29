7ede6168fdc9e42658b0cd12885f6fd9a377343b
Emily Patonay <espatonay@gmail.com>
2015-05-29 08:24:28 +0200

    Start Button starts keyboard listener

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,20 @@
 
 // OTHER FUNCTIONS
 (function($){
+    app.CONSTANTS = {
+        ESC_KEY: $.ui.keyCode.ESCAPE,
+        83: 'S',
+        65: 'A',
+        68: 'D',
+        70: 'F',
+        90: 'Z',
+        88: 'X',
+        67: 'C',
+        86: 'V',
+        66: 'B'
+
+    };
+
     app.tools = {
         tabbedOutput: function(){
             $('ul.tabs li').click(function() {
@@ -22,7 +36,6 @@
         resetCounter: function(){
             // This will allow the counters to all be set to 0
             // to start the script
-            console.log('Resetting counter');
             $('.cellAmount').val(0);
             $('.percentCell').text(0);
             $('#percentcelltot').text('100%');
@@ -44,6 +57,39 @@
             console.log('Hiding ' + hideType);
             $('.' + toType).removeClass('hidden');
             $('.' + hideType).addClass('hidden');
+        }
+    };
+
+    app.utils = {
+        btnStart: function(){
+            $('#buttons').on('click', '#btnStartCount', function(){
+                console.log("Starting counting.");
+
+                /*
+                * Whenever the user clicks "Start Count," disable the button.
+                * This prevents each cunt being rgistered as may times as the start
+                * button is pressed. If you would like to play a practical joke on
+                * the poor user, then trigger the start button multiple times.
+                */
+                document.getElementById('btnStartCount').disabled = true;
+
+                /* Listen to keyboard keypresses. Binding to document level so that
+                 I don't have to worry about setting focus to anything.
+                  */
+
+
+                $(document).keydown(function(ev){
+                    // First, make sure escape key doesn't do anything untoward
+                    var x = app.CONSTANTS[ev.which];
+                    console.log(x + ' ' + _.isString(x));
+
+                    // Fire up the KeyPress library
+                    var listener = new window.keypress.Listener();
+                    listener.simple_combo(x, function(){
+                        console.log('pressed ');
+                    });
+                });
+            })
         }
     };
 })(jQuery);
